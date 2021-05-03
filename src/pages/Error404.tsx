b326f3029d287b94fde9b6f5305f4f46b702272e
geraud.henrion <geraud.henrion@nartex.fr>
2021-05-04 00:30:06 +0200

    Update styles

--- src/pages/Error404.tsx
+++ src/pages/Error404.tsx
@@ -1,5 +1,5 @@
-import { IonButton, IonContent, IonPage } from "@ionic/react";
-import { Link, useRouteMatch } from "react-router-dom";
+import { IonButton, IonContent, IonFooter, IonPage } from "@ionic/react";
+import { useRouteMatch } from "react-router-dom";
 import { Header } from "../components/Header";
 
 export function Error404() {
@@ -13,12 +13,14 @@ export function Error404() {
         <div className="container">
           <h1>404</h1>
           <h2>La page "{match.path}" est introuvable</h2>
-
-          <Link to="/">
-            <IonButton>Retour à l'accueil</IonButton>
-          </Link>
         </div>
       </IonContent>
+
+      <IonFooter>
+        <IonButton expand="full" routerLink="/" routerDirection="root">
+          Retour à l'accueil
+        </IonButton>
+      </IonFooter>
     </IonPage>
   );
 }
