cb902562ae02fa02d13926f17797a5aa3fe9a381
geraud.henrion <geraud.henrion@nartex.fr>
2021-05-04 00:08:42 +0200

    Calculate position between position and dest

--- src/pages/Travel/Map.jsx
+++ src/pages/Travel/Map.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import ReactMapGL, { Marker } from "react-map-gl";
 
 import "mapbox-gl/dist/mapbox-gl.css";
@@ -6,6 +6,8 @@ import "./Map.css";
 import { MAPBOX_ACCESS_TOKEN } from "../../config";
 import cascade from "./cascade.svg";
 import { useRealPosition, useSimulatedPosition } from "./usePosition";
+import distance from "@turf/distance";
+import { point } from "@turf/helpers";
 
 const mapStyleUrl =
   "https://api.mapbox.com/styles/v1/mapbox/cjtep62gq54l21frr1whf27ak";
@@ -14,17 +16,31 @@ export function Map() {
   const [viewport, setViewport] = useState({
     latitude: 47.7386289,
     longitude: 7.3293385,
-    zoom: 12,
+    zoom: 15,
   });
 
   const destination = {
-    // Le KM0
     latitude: 47.7386289,
     longitude: 7.3293385,
-  };
+  }
 
   const currentPosition = useSimulatedPosition();
   // const currentPosition = useRealPosition();
+
+  const distanceToDestination = currentPosition
+    ? distance(
+        point([destination.latitude, destination.longitude]),
+        point([currentPosition.latitude, currentPosition.longitude]),
+        { units: "meters" }
+      )
+    : Infinity;
+  const isCloseEnough = distanceToDestination < 100;
+
+  useEffect(() => {
+    if(isCloseEnough) {
+      alert('Good game')
+    }
+  }, [isCloseEnough])
 
   return (
     <ReactMapGL
