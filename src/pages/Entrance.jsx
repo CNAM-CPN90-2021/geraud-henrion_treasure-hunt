c111a00252117938b9c2a44ad602208a2bfa366f
geraud.henrion <geraud.henrion@nartex.fr>
2021-06-02 10:47:20 +0200

    Add qrcode scanner

--- src/pages/Entrance.jsx
+++ src/pages/Entrance.jsx
@@ -1,7 +1,11 @@
-import { IonButton } from "@ionic/react";
+import { IonButton, useIonToast } from "@ionic/react";
 import { PageLayout } from "../components/PageLayout";
+import { useQRCodeScanner } from "../useQRCodeScanner";
 
 export function Entrance() {
+  const { startScan } = useQRCodeScanner();
+  const [showToast] = useIonToast();
+
   return (
     <PageLayout
       title="L'entrée de la vallée"
@@ -25,6 +29,23 @@ export function Entrance() {
           s'ouvrira. // TODO
         </code>
       </p>
+      <IonButton
+        onClick={async () => {
+          const result = await startScan();
+
+          console.log(result);
+          if (result.content === "good_answer") {
+            showToast("Bravo, bonne réponse", 2000)
+          } else if (result.content === "wrong_answer") {
+            showToast("Eh non, mauvaise réponse", 2000);
+          } else {
+            showToast("Code non reconnu", 2000);
+          }
+
+        }}
+      >
+        Scanner le cuillère-code
+      </IonButton>
     </PageLayout>
   );
 }
